8ed24f4f35f41250da2cb39f95e53b865c8f62ea
Üzeyir Cenker Arda <202104115040@dogus.edu.tr>
2023-01-23 03:35:12 +0300

    Update Görev.cs
    a

--- AlegoriaSon-main/Assets/Cenker Kodlar/Görev.cs
+++ AlegoriaSon-main/Assets/Cenker Kodlar/Görev.cs
@@ -11,11 +11,14 @@ public class Görev : MonoBehaviour
     public GameObject ilkObj;
     public GameObject ikinciObj;
     public GameObject ucuncuObj;
+    public GameObject dorduncuObj;
     bool basladımı;
     public int sayac;
     [Header("Uzaklığa Göre Çalışma")]
     public Transform hedef;
+    public Transform player;
     public float menzil;
+
     // Start is called before the first frame update
     public void GörevYapıldımı(GameObject bbba)
     {
@@ -32,7 +35,11 @@ public class Görev : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        menzil = Vector3.Distance( hedef.position, transform.position);
+        menzil = Vector3.Distance(player.position,hedef.position);
+        if (menzil <= 2.5f && Input.GetKeyDown(KeyCode.E))
+        {
+            sayac++;
+        }
         if (basladımı)
         {
             if (sayac == 1)
@@ -44,28 +51,28 @@ public class Görev : MonoBehaviour
             {
                 görevText.text = "Anahtarları Al";
                 GörevYapıldımı(ilkObj);
+                hedef.position = ikinciObj.transform.position;
             }
             if (sayac == 3)
             {
                 görevText.text = "Koltukların Üstündeki Örtüleri Kaldır";
                 GörevYapıldımı(ikinciObj);
-
+                hedef.position = ucuncuObj.transform.position;
             }
             if (sayac == 4)
             {
                 görevText.text = "Işığı Aç, Güneş Batıyor";
                 GörevYapıldımı(ucuncuObj);
+                hedef.position = dorduncuObj.transform.position;
             }
         }
+
     }
-    private void OnMouseOver()
+    private void PressE()
     {
-        if (menzil <= 2.5f)
+        if (menzil <= 2.5f && Input.GetKeyDown(KeyCode.E))
         {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
                 sayac++;
-            }
         }
 
     }
